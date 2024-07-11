7ff6253bdba495a0680b47774b81e9b924b1399c
anderson-Gm <anderson.garcigrado@gmail.com>
2024-07-11 11:50:42 -0300

    meu-projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+var Pokemon = "Charizard"
+var estagio = "Ferro"
+var xp = 170
+var evolucaoAnterior = "Chameleon"
+var Habilidade = "intuição de Batalha"
+var ataque = "Labareda Real: Este ataque causa 50 pontos de dano"
+var fraqueza = "Água"
+
+console.log("Pokemon: " + Pokemon + "Estágio " +estagio + " Evolui de: " + evolucaoAnterior + 
+    " habilidade: " + Habilidade + " Principal ataque: " +
+    ataque + " Fraqueza: " + fraqueza
+)
